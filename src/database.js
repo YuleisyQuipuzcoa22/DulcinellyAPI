75122796fc6b3671cc006522dd240827dbb31eec
YuleisyQuipuzcoa22 <yuleisyquipuzcoa@gmail.com>
2026-05-31 00:59:41 -0500

    nuevas credenciales

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 import {createPool} from 'mysql2/promise'
 export const pool=createPool({
-    host:'turntable.proxy.rlwy.net',
+    host:'zephyr.proxy.rlwy.net',
     user:'root',
-    password:'iUnjzqqnEOlvNKZDotcEWBIgBnIyljEO',
+    password:'QdkNaQrwvVBWaFlbuUszcutfjdgfPxGi',
     database:'Dulcinelly',
-    port:39651,
+    port:16509,
 })
